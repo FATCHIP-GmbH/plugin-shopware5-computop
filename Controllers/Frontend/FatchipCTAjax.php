653e02c39d95a270d624023b6b1178c17e017b89
stefan <stefan.mueller@fatchip.de>
2018-02-08 08:32:01 +0100

    0035882: fixed SOD and GOD response status

--- Controllers/Frontend/FatchipCTAjax.php
+++ Controllers/Frontend/FatchipCTAjax.php
@@ -55,7 +55,7 @@ class Shopware_Controllers_Frontend_FatchipCTAjax extends Enlight_Controller_Act
         $response = $service->callComputopAmazon($requestParams);
         $data = [];
         $data['data'] = $response;
-        $data['status'] =($response['Status'] == 'OK' ? 'success' : 'error');
+        $data['status'] =($response['Status'] == 'AUTHORIZE_REQUEST' ? 'success' : 'error');
         $data['errormessage'] = $response['Description'];
         $encoded = json_encode($data);
         echo $encoded;
@@ -82,7 +82,7 @@ class Shopware_Controllers_Frontend_FatchipCTAjax extends Enlight_Controller_Act
         $response = $service->callComputopAmazon($requestParams);
         $data = [];
         $data['data'] = $response;
-        $data['status'] = ($response['Status'] == 'OK' ? 'success' : 'error');
+        $data['status'] = ($response['Status'] == 'AUTHORIZE_REQUEST' ? 'success' : 'error');
         $data['errormessage'] = $response['Description'];
         $encoded = json_encode($data);
         echo $encoded;
