4fbec34ecba121429806173d78edb993656022ba
stefan <stefan.mueller@fatchip.de>
2018-03-23 15:47:08 +0100

    0039354: Computop - Lizenzmodel

--- Views/responsive/frontend/_resources/javascript/fatchipCTCreditCard.js
+++ Views/responsive/frontend/_resources/javascript/fatchipCTCreditCard.js
@@ -1,4 +1,4 @@
-$.plugin('fatchipCTCreditCard', {
+$.plugin("fatchipCTCreditCard", {
     defaults: {
         fatchipCTCCNr: false,
         fatchipCTCCCVC: false,
@@ -7,62 +7,79 @@ $.plugin('fatchipCTCreditCard', {
     },
 
     init: function () {
+        "use strict";
         var me = this;
         me.applyDataAttributes();
-        console.log('fatchipCTCCNr:');
+        console.log("fatchipCTCCNr:");
         console.log(me.opts.fatchipCTCCNr);
-        console.log('fatchipCTCCCVC:');
+        console.log("fatchipCTCCCVC:");
         console.log(me.opts.fatchipCTCCCVC);
-        console.log('fatchipCTCCExpiry:');
+        console.log("fatchipCTCCExpiry:");
         console.log(me.opts.fatchipCTCCExpiry);
-        console.log('fatchipCTCCBrand:');
+        console.log("fatchipCTCCBrand:");
         console.log(me.opts.fatchipCTCCBrand);
+        //me.preventFormSubmit();
+    },
+
+    // triggered by init, when payment choice changes to "Computop Kreditkarte"
+    preventFormSubmit: function () {
+        "use strict";
+        console.log("triggered preventFormSubmit");
+        //fatchipCTShippingPayment.preventFormSubmit();
+        console.log("after preventFormSubmit");
     },
 
     destroy: function () {
+        "use strict";
         var me = this;
         me._destroy();
     }
 });
 
-$.plugin('fatchipCTSubmitCCForm', {
+$.plugin("fatchipCTShippingPayment", {
     defaults: {
-        fatchipCTCCNr: false,
-        fatchipCTCCCVC: false,
-        fatchipCTCCExpiry: false,
-        fatchipCTCCBrand: false
+        submit: false
     },
 
     init: function () {
+        "use strict";
         var me = this;
         me.applyDataAttributes();
-        console.log('fatchipCTSubmitCCForm INIT:');
-        $("#shippingPaymentForm").on('submit', function (e) {
-            console.log('before preventDefault');
+        console.log("fatchipCTSubmitCCForm INIT:");
+    },
+
+    preventFormSubmit: function () {
+        "use strict";
+        var me = this;
+        me.applyDataAttributes();
+        console.log("fatchipCTSubmitCCForm INIT:");
+        me.$el.on("submit", function (e) {
+            console.log("before FatchipCTShippingPayment");
             e.preventDefault();
-            console.log('after preventDefault');
+            console.log("after FatchipCTShippingPayment");
 
         });
     },
 
-
     destroy: function () {
-        comnsole.log('destroy Plugin');
+        "use strict";
+        console.log("destroy Plugin triggered");
         var me = this;
         me._destroy();
     }
 });
 
-/*function poBindDispatchChange() {
-    $("input[name='sDispatch']").on('change', function (e) {
+function fatchipCTInputChanged() {
+    "use strict";
+    $("input[name=\"sDispatch\"]").on("change", function (e) {
+        console.log("Dispatch changed");
         return true;
     });
 }
 
 $.subscribe("plugin/swShippingPayment/onInputChanged", function () {
-    poBindDispatchChange();
+    "use strict";
+    fatchipCTInputChanged();
 });
-*/
 
-$('#fatchipCTCreditCard').fatchipCTCreditCard();
-$('#shippingPaymentForm').fatchipCTSubmitCCForm();
+$("#fatchipCTCreditCard").fatchipCTCreditCard();
