9b97110f76e25a823dee2514ae479da33d49510e
stefan <stefan.mueller@fatchip.de>
2018-04-04 12:42:16 +0200

    0039354: cleaned up logging

--- Views/responsive/frontend/_resources/javascript/fatchipCTCreditCard.js
+++ Views/responsive/frontend/_resources/javascript/fatchipCTCreditCard.js
@@ -9,11 +9,6 @@ $.plugin("fatchipCTCreditCardIFrame", {
         "use strict";
         var me = this;
         me.applyDataAttributes();
-        console.log(me.opts.fatchipCTCreditcardIFrameUrl);
-        console.log("fatchipCTErrorMessage:");
-        console.log(me.opts.fatchipCTErrorMessage);
-        console.log("fatchipCTErrorCode:");
-        console.log(me.opts.fatchipCTErrorCode);
 
         window.top.location.href = me.opts.fatchipCTCreditcardIFrameUrl + "?CTError[CTErrorMessage]=" + me.opts.fatchipCTErrorMessage + "&CTError[CTErrorCode]=" + me.opts.fatchipCTErrorCode;
     },
