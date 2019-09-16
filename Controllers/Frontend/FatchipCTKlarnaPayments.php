2d2321cd3c5a2f5eda7eafb058d70370f59fd6af
Stephan Altmann <stephan.altmann@fatchip.de>
2019-09-16 15:26:34 +0200

    prepared basic klarna session request

--- Controllers/Frontend/FatchipCTKlarnaPayments.php
+++ Controllers/Frontend/FatchipCTKlarnaPayments.php
@@ -51,137 +51,18 @@ class Shopware_Controllers_Frontend_FatchipCTKlarnaPayments extends Shopware_Con
      */
     public $paymentClass = 'KlarnaPayments';
 
-    /**
-     * GatewayAction is overridden because there is no redirect but a server to server call is made
-     *
-     * On success create the order and forward to checkout/finish
-     * On failure forward to checkout/payment and set the error message
-     *
-     * @return void
-     * @throws Exception
-     */
-    public function gatewayAction()
-    {
-        // getPaymentClassForGatewayAction is overridden
-        $payment = $this->getPaymentClassForGatewayAction();
-        $requestParams = $payment->getRedirectUrlParams();
-        $response = $this->plugin->callComputopService($requestParams, $payment, 'KLARNA', $payment->getCTPaymentURL());
+    protected function getKlarnaSessionAction() {
+        $payment = $this->paymentService->getPaymentClass('KlarnaPayments', $this->config);
 
-        switch ($response->getStatus()) {
-            case CTEnumStatus::OK:
-                $orderNumber = $this->saveOrder(
-                    $response->getTransID(),
-                    $response->getPayID(),
-                    self::PAYMENTSTATUSRESERVED
-                );
-                $this->saveTransactionResult($response);
-
-                $customOrdernumber = $this->customizeOrdernumber($orderNumber);
-                $this->updateRefNrWithComputopFromOrderNumber($customOrdernumber);
-                $this->forward('finish', 'checkout', null, ['sUniqueID' => $response->getPayID()]);
-                break;
-            default:
-                $ctError = [];
-                $ctError['CTErrorMessage'] = self::ERRORMSG; // . $response->getDescription();
-                $ctError['CTErrorCode'] = ''; //$response->getCode();
-                return $this->forward('shippingPayment', 'checkout', null, array('CTError' => $ctError));
-
-                break;
-        }
-    }
-
-    /**
-     * GetPaymentClassForGatewayAction is overridden because call to
-     * getIframePaymentClass has no parameters
-     * URLSuccess, URLFailure, isFirm and $klarnaAction parameters
-     *
-     * Furthermore SSN, AnnualSalary, Phone and DoB need to be set
-     *
-     * @return KlarnaPayments
-     * @throws Exception
-     */
-    protected function getPaymentClassForGatewayAction()
-    {
-        $orderVars = $this->session->sOrderVariables;
-        $userData = $orderVars['sUserData'];
-
-        $ctOrder = $this->createCTOrder();
-
-        $usesInvoice = ($userData['additional']['payment']['name'] === 'fatchip_computop_klarna_invoice');
-        $isFirm = !empty($userData['billingaddress']['company']);
-        $klarnaAction = $usesInvoice ? '-1' : $this->config['klarnaaction'];
-
-        /**
-         * Klarna Payment Class.
-         *
-         * @var KlarnaPayments $payment
-         */
-        $payment = $this->paymentService->getIframePaymentClass(
-            $this->paymentClass,
-            $this->config,
-            $ctOrder,
-            null,
-            null,
-            $this->router->assemble(['action' => 'notify', 'forceSecure' => true]),
+        $requestParams = $payment->getKlarnaSessionRequestParams(
+            $session->offsetGet('fatchipCTPaymentPayID'),
+            $session->offsetGet('fatchipCTPaymentTransID'),
+            $this->getAmount() * 100,
+            $this->getCurrencyShortName(),
             $this->getOrderDesc(),
-            $this->getUserDataParam(),
-            null,
-            $isFirm,
-            $klarnaAction
+            $session->offsetGet('fatchipCTAmazonReferenceID')
         );
 
-        $payment->setSocialSecurityNumber($this->utils->getUserSSN($userData));
-        $payment->setAnnualSalary($this->utils->getUserAnnualSalary($userData));
-        $payment->setPhone($this->utils->getUserPhone($userData));
-        $payment->setDateOfBirth($this->utils->getUserDoB($userData));
-
-        return $payment;
+        $response = $this->plugin->callComputopService($requestParams, $payment, 'SCO', $payment->getCTPaymentURL());
     }
-
-
-    /**
-     * Beschreibung der gebuchten Artikel:
-     * Menge, ArtikelNr, Bezeichnung, Preis, Arti-kelkennung. Rabatt und MwSt. als Prozentzahl angeben.
-     * Felder durch ";" und Rechnungspositionen durch "+" trennen.
-     * Preise ohne Komma in kleinster Wäh-rungseinheit angeben:
-     * <qty>;<artno>; <title>; <price>; <vat>;<discount>;<Artic-leFlag> +
-     *
-     * Beispiel: 25;12345;Kugelschreiber;890;19;1.5;0 + 1;11223;Versandkosten;490;19;0;8
-     *
-     * Werte und Wirkung des <ArticleFlag>:
-     * <0> keine Kennzeichnung,
-     * <1> Mengen-angabe in 1/1000,
-     * <2> Menge in 1/100,
-     * <4> Menge in 1/10,
-     * <8> Artikel ist eine Versandgebühr,
-     * <16> Artikel ist eine Bearbeitungsgebühr,
-     * <32> Preisangabe erfolgt inkl. MwSt.
-     *
-     * @return string
-     */
-    public function getOrderDesc()
-    {
-        $basket = $this->getBasket();
-        $orderDesc = '';
-        foreach ($basket['content'] as $position) {
-            if (!empty($orderDesc)) {
-                $orderDesc .= ' + ';
-            }
-            //careful: $position['amount'] contains the total for the position, so QTY*Price
-            //in controllers/backend/FatchipCTOrder $position->getPrice() returns price of only 1 article
-            $orderDesc .= $position['quantity'] . ';' . $position['articleID'] . ';' . $position['articlename'] . ';'
-                . $position['amount'] * 100 / $position['quantity'] . ';' . $position['tax_rate'] . ';0;0';
-        }
-        //add shipping if > 0
-        if ($basket['sShippingcosts'] != 0) {
-            if (!empty($orderDesc)) {
-                $orderDesc .= ' + ';
-            }
-            $orderDesc .= '1;shipping;Versandkosten;' . $basket['sShippingcosts'] * 100 . ';' . $basket['sShippingcostsTax'] . ';0;8';
-        }
-
-        return $orderDesc;
-    }
-
-
 }
